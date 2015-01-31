18252813c362dbfcebb2814677c57ae8a484a16a
Masato Oshima <qqw75pn9@gmail.com>
2015-02-01 03:34:44 +0900

    Initial Commit

---
+++ TransitionSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionSample
+//
+//  Created by Masato Oshima on 2015/02/01.
+//  Copyright (c) 2015年 Masato Oshima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransitionSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionSample
+//
+//  Created by Masato Oshima on 2015/02/01.
+//  Copyright (c) 2015年 Masato Oshima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
